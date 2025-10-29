d91423f76b96ba548a8e63706771ef81347ab857
alnesssa <_EMAIL__GITHUB>
2025-10-29 17:25:36 +0500

    Task7and9

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,12 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const soundBtn = document.getElementById("soundBtn");
-  const sound = new Audio("../sounds/notification.mp3");
+$(document).ready(function () {
+  console.log("jQuery is ready!");
 
-  soundBtn.addEventListener("click", () => {
-    sound.currentTime = 0;
-    sound.play();
+  $("#loginForm").on("submit", function (e) {
+    e.preventDefault();
 
-    soundBtn.classList.add("active");
-    setTimeout(() => soundBtn.classList.remove("active"), 200);
+    const username = $("#username").val().trim();
+    const password = $("#password").val().trim();
+    const savedUser = JSON.parse(localStorage.getItem("user"));
+
+    if (savedUser && username === savedUser.username && password === savedUser.password) {
+      
+      // Play sound only if success
+      const sound = new Audio("../sounds/notification.mp3");
+      sound.currentTime = 0;
+      sound.play();
+
+      // Add login status
+      localStorage.setItem("loggedIn", "true");
+
+      // Toast visible
+      $("#toast").css("opacity", "1");
+
+      // Redirect after fade
+      setTimeout(() => {
+        $("#toast").css("opacity", "0");
+        window.location.href = "../pages/aliya-profile.html";
+      }, 1500);
+
+    } else {
+      $("#error-message").text("Invalid username or password");
+    }
   });
 });
